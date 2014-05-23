1402372ab1fcf6699e6fbd6b52dc19f3c333e472
andyjames250 <andyjames250@gmail.com>
2014-05-23 12:18:02 +1000

    Finished importGoogleDrive2

--- getGoogleDrive.R
+++ getGoogleDrive.R
@@ -1,78 +1,63 @@
 downloadGoogleDrive <- function(output, url, key) {
         
         ## This function downloads a text file from Google Drive into a local text file
+        ## Note: Google Drive file must be shared to anyone with the link
         ## Arguments:
         ## output = output file name
         ## url = Google Drive shared document URL
         ## key = Google Drive document key
-        ## Note: Google Drive file must be shareable
         ## Note: This function will try to use url first, then try key
-        
+        ## Note: This function will fail for any file that is too large for Google's virus scan        
+
+        require(RCurl)
         if(!missing(url)) {
                 link <- sub("/edit?usp=sharing", "", sub("/file/d/", "/uc?export=download&id=", url, fixed = TRUE), fixed = TRUE)
         } else {
                 link <- paste0("https://drive.google.com/uc?export=download&id=", key)
         }
-        require(RCurl)
         bin <- getBinaryURL(link, followLocation = TRUE, ssl.verifypeer = FALSE)
         writeBin(bin, output, useBytes = TRUE)
         message(noquote(paste(output, "read into", getwd())))                        
 }
 
-importGoogleDrive <- function(url, key, sep = "\t") {
+importGoogleDrive <- function(url, key, sep = "\t", nrows = 1000) {
         
-        ## This function imports a text file from Google Drive into a data frame
+        ## This function imports a delimited text file from Google Drive into a data frame
+        ## Note: Google Drive file must be shared to anyone with the link
         ## Arguments:
         ## url = Google Drive shared document URL
         ## key = Google Drive document key
         ## sep = delimiter for read.csv function
-        ## Note: Google Drive file must be shareable
         ## Note: This function will try to use url first, then try key
-        
+        ## Note: This function will fail for any file that is too large for Google's virus scan
+
+        require(RCurl)
         if(!missing(url)) {
                 link <- sub("/edit?usp=sharing", "", sub("/file/d/", "/uc?export=download&id=", url, fixed = TRUE), fixed = TRUE)
         } else {
                 link <- paste0("https://drive.google.com/uc?export=download&id=", key)
         }
-        require(RCurl)
         bin <- getBinaryURL(link, followLocation = TRUE, ssl.verifypeer = FALSE)
-        dataFrame <- read.csv(textConnection(readBin(bin, character())), sep = sep)
+        dataFrame <- read.csv(textConnection(readBin(bin, character())), sep = sep, nrows = nrows)
 }
 
-importGoogleDrive2 <- function(url, key, sep = "\t", nrows = 1000) {
+importGoogleDrive2 <- function(url, key, filePath, sep = "\t", nrows = 1000) {
         
-        ## This function imports a text file from Google Drive into a data frame
-        ## Arguments:
-        ## url = Google Drive shared document URL
-        ## key = Google Drive document key
-        ## sep = delimiter for read.csv function
-        ## Note: Google Drive file must be shareable
+        ## This function imports a delimited text file from Google Drive into a data frame
+        ## Note: Google Drive folder must be public
         ## Note: This function will try to use url first, then try key
+        ## Arguments:
+        ## url = Google Drive shared folder URL
+        ## key = Google Drive folder key
+        ## filePath = path and filename relative to Google Drive shared folder
+        ## sep = delimiter for read.csv function
         
-        require(stringr)
         require(RCurl)
-        
         if(!missing(url)) {
-                link <- sub("/edit?usp=sharing", "", sub("/file/d/", "/uc?export=download&id=", url, fixed = TRUE), fixed = TRUE)
+                link <- sub("&usp=sharing", filePath, sub("drive.google.com/folderview?id=", "googledrive.com/host/", url, fixed = TRUE), fixed = TRUE)
         } else {
-                link <- paste0("https://drive.google.com/uc?export=download&id=", key)
+                link <- paste0("https://googledrive.com/host/", key, filePath)
         }
-
         bin <- getBinaryURL(link, followLocation = TRUE, ssl.verifypeer = FALSE)
-        result <- readBin(bin, character())
-        confirmRegex <- "confirm=[[:alnum:]]{1,}&"
-        m <- regexpr(confirmRegex, result)
-        if(m[1] > -1) {
-                confirmText <- regmatches(result, m)
-#                 confirmCode1 <- str_extract(x, confirmRegex)
-#                 confirmCode2 <- strsplit(confirmCode1, "=")
-#                 confirmCode3 <- confirmCode[[1]][2]
-#                 confirmCode4 <- strsplit(confirmCode3, "&")
-#                 confirmCode5 <- confirmCode4[[1]][1]
-                link <- sub("id=", paste0(confirmText, "id="), link, fixed = TRUE)
-                bin2 <- getBinaryURL(link, followLocation = TRUE, ssl.verifypeer = FALSE)
-                dataFrame <- read.csv(textConnection(readBin(bin2, character())), sep = sep, nrows = nrows)
-        } else {
-                dataFrame <- read.csv(textConnection(readBin(bin, character())), sep = sep, nrows = nrows)
-        }
+        dataFrame <- read.csv(textConnection(readBin(bin, character())), sep = sep)
 }
